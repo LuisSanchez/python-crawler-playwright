456774a865410db96ade07243f69cd94b903fa8b
Luis Manuel Sanchez Marquez <luissanchez@outlook.com>
2023-12-10 21:52:43 -0300

    [chore]:minor comments

--- start.py
+++ start.py
@@ -5,6 +5,7 @@ from linkedin_scrapper import scrape_linkedin
 def start():
     companies = file_reader()
     # Replace with your LinkedIn credentials
+    # This could be added to a .env file or a config yaml file, but for simplicity I will leave it here
     username = ""
     password = ""
     scrape_linkedin(companies, username, password)
